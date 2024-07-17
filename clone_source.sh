fe993e169e5d259018378ae41033263fe42d07f5
秋野かえで <autmaple@protonmail.com>
2024-07-17 16:31:16 +0000

    fix feeds

--- clone_source.sh
+++ clone_source.sh
@@ -1,6 +1,8 @@
 git clone "https://github.com/coolsnowwolf/lede.git" --branch "master" --single-branch "lede"
 pushd "lede"
-echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git kaede https://github.com/AkinoKaede/openwrt-packages" >> "feeds.conf.default"
+echo "src-git daed https://github.com/QiuSimons/luci-app-daed" >> "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld" >> "feeds.conf.default"
 ./scripts/feeds update -a; ./scripts/feeds install -a;
 echo '
 
