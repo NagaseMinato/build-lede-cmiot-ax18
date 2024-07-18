c9128ea6e5c198d0410f38b045e3b4e186314d2b
秋野かえで <autmaple@protonmail.com>
2024-07-18 13:29:55 +0000

    update

--- clone_source.sh
+++ clone_source.sh
@@ -4,13 +4,4 @@ echo "src-git kaede https://github.com/AkinoKaede/openwrt-packages" >> "feeds.co
 echo "src-git daed https://github.com/QiuSimons/luci-app-daed" >> "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld" >> "feeds.conf.default"
 ./scripts/feeds update -a; ./scripts/feeds install -a;
-echo '
-
-config KERNEL_XDP_SOCKETS
-	bool "XDP sockets support"
-	default y if KERNEL_DEBUG_INFO_BTF
-	help
-	  XDP sockets allows a channel between XDP programs and
-	  userspace applications.
-'>> "config/Config-kernel.in"
 popd
